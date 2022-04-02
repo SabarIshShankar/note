5c85d67b7cf36a85fff682581e9f1b1065552239
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-04-02 15:15:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require("path")
+const fs = require("fs")
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser =  require("body-parser")
+const cookieParser = require("cookie-parser")
+const multer =  require("multer")
+const dotenv = require("dotenv")
+dotenv.config();
+
